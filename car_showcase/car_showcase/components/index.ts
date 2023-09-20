6f67361e9c7000688cb93ea4a2a6ed9f7ecb2d50
Chaïner KOMISSA ZOTSU <essikomissa@gmail.com>
2023-09-20 19:36:29 +0000

    Commit 3
    Search bar

--- car_showcase/car_showcase/components/index.ts
+++ car_showcase/car_showcase/components/index.ts
@@ -2,7 +2,10 @@ import Hero from "./hero";
 import CustomButton from "./customButton";
 import Navbar from "./navbar";
 import Footer from "./footer";
+import CustomFilter from "./customFilter";
+import SearchBar from "./searchBar";
+import SearchManufacturer from "./searchManufacturer";
 
 export {
-    Hero, CustomButton, Navbar, Footer
+    Hero, CustomButton, Navbar, Footer, CustomFilter, SearchBar, SearchManufacturer
 }
