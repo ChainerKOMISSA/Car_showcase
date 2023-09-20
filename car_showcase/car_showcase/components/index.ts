68814be6c3ac91f5bbd64cf7f1d864efea12e9cb
Chaïner KOMISSA ZOTSU <essikomissa@gmail.com>
2023-09-20 12:53:06 +0000

    Commit 2
    Header, Navbar et footer

--- car_showcase/car_showcase/components/index.ts
+++ car_showcase/car_showcase/components/index.ts
@@ -1,6 +1,8 @@
 import Hero from "./hero";
 import CustomButton from "./customButton";
+import Navbar from "./navbar";
+import Footer from "./footer";
 
 export {
-    Hero, CustomButton
+    Hero, CustomButton, Navbar, Footer
 }
